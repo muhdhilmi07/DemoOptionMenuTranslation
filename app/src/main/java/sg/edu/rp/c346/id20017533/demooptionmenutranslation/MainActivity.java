abe61f290b02856eb5a0dffba1eb876b06f5dad9
20017533 <20017533@myrp.edu.sg>
2021-05-27 15:56:37 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/id20017533/demooptionmenutranslation/MainActivity.java
@@ -0,0 +1,49 @@
+package sg.edu.rp.c346.id20017533.demooptionmenutranslation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView tvTranslatedText;
+    String wordClicked = "";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tvTranslatedText = findViewById(R.id.textViewTranslatedText);
+
+        registerForContextMenu(tvTranslatedText);
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.EnglishSelection) {
+            tvTranslatedText.setText("Hello ");
+            return true;
+        } else if (id == R.id.italianSelection) {
+            tvTranslatedText.setText("Ciao");
+        }
+            return super.onOptionsItemSelected(item);
+
+
+    }
+
+}
